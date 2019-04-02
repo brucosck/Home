99149f04369dd2bebecbb8aca3e1a377b2ccb334
Bruno Caldeira <brucosck@gmail.com>
2019-04-02 12:26:42 -0300

    dev2

--- install.sh
+++ install.sh
@@ -12,13 +12,30 @@ apt install -y xorg i3 \ # interface grafica i3wm
                feh \ #suporte a papel de parede
                dirmngr \ #necessario para adicionar chaves no source.list
                sudo \ #permite o uso do sudo
-               
-               
+               git \ #necessario para baixar conteudo do github
+               wget \ #necessario se necessario baixar algum arquivo
+dpkg --add-architecture i386 #Necessario para instalação da Steam
+
 #Adicionando Repositórios
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' #Necessario para instalação do Google Chrome
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add - #Necessario para instalação do Google Chrome
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A87FF9DF48BF1C90 #Necessario para intalação spotify
 echo deb http://repository.spotify.com stable non-free | tee /etc/apt/sources.list.d/spotify.list #Necessario para instalação spotify
+
+#ATENÇÃO Necessario para instalação da steam
+#aqui o script adiciona 'contrib' e 'non-free' as fontes correspondentes abaixo.
+#caso vc utilize outras fontes devera adicionar manualmente.
+
+#deb http://ftp.br.debian.org/debian/ stretch main contrib non-free
+#deb http://security.debian.org/debian-security stretch/updates main contrib non-free
+#deb http://ftp.br.debian.org/debian/ stretch-updates main contrib non-free
+sed -i "s/deb http:/\/\ftp.br.debian.org/\debian/\ stretch main/deb http:/\/\ftp.br.debian.org/\debian/\ stretch main contrib non-free/g" /etc/apt/sources.list
+sed -i "s/deb http:/\/\security.debian.org/\debian-security stretch/\updates main/deb http:/\/\security.debian.org/\debian-security stretch/\updates main contrib non-free/g" /etc/apt/sources.list
+sed -i "s/deb http:/\/\ftp.br.debian.org/\debian/\ stretch-updates main/deb http:/\/\ftp.br.debian.org/\debian/\ stretch-updates main contrib non-free/g" /etc/apt/sources.list
 
 
 apt update
 
-#need package
-apt install spotify-client
+#need package spotify
+apt install google-chrome-stable spotify-client steam
