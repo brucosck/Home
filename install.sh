62f0749c8c5d04b595acd6d59f3d0f327f30091e
Bruno Caldeira <brucosck@gmail.com>
2019-04-03 08:24:12 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 # Executar apos a instalação limpa do Debian 9 stretch amd64
 
 apt update
-apt install -y xorg i3 \ # interface grafica i3wm
-               slim \ # gerenciador de Login
-               alsautils pavucontrol pulseaudio-utils \ # audo para a placa de som CA0108/CA10300 | Sound Blaster Audigy Series SB0610
-               dunst libnotify-bin \ # sistema de notificação.
-               rofi \ # Lançador
+apt install -y xorg i3 \ #interface grafica i3wm
+               slim \ #gerenciador de Login
+               alsautils pavucontrol pulseaudio-utils \ #audo para a placa de som CA0108/CA10300 | Sound Blaster Audigy Series SB0610
+               dunst libnotify-bin \ #sistema de notificação.
+               rofi \ #Lançador
                curlftpfs \ #ftp como diretório do sistema
                feh \ #suporte a papel de parede
                dirmngr \ #necessario para adicionar chaves no source.list
