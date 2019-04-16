71bb7a72147f9ce7c0840f3492063f6eaf5d61e2
Bruno Caldeira <brucosck@gmail.com>
2019-04-16 20:04:03 -0300

    Update installChrome.sh

--- installChrome.sh
+++ installChrome.sh
@@ -3,7 +3,7 @@
 apt install dirmngr
 
 #Necessario para instalação do Google Chrome
-sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' 
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list' 
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add - 
 
 apt update
