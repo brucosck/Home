dbcde1b6c74df88887300bb3212d61419d9e8015
Bruno Caldeira <brucosck@gmail.com>
2019-04-17 12:20:49 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,7 @@
 
 # Executar apos a instalação limpa do Debian 9 stretch amd64
 
+dpkg --add-architecture i386
+
 apt update
 apt install -y xorg i3 slim alsa-utils pavucontrol pulseaudio-utils dunst libnotify-bin rofi curlftpfs feh sudo conky make dirmngr
