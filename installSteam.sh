00cc9c60f813a33a0cd2b617978322888274615b
Bruno Caldeira <brucosck@gmail.com>
2019-04-16 22:00:22 -0300

    Update installSteam.sh

--- installSteam.sh
+++ installSteam.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+dpkg --add-architecture i386
+
 echo "deb http://ftp.br.debian.org/debian stretch main contrib non-free
 deb http://security.debian.org/ stretch/updates main contrib non-free
 deb http://ftp.br.debian.org/debian/ stretch-updates main contrib non-free" > /etc/apt/sources.list.d/steam.list
-
-dpkg --add-architecture i386
 
 apt update 
 
